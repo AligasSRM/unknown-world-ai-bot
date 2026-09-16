7ae8d04f04009450d40a2fd8e9f75b28e350f9b0
AligasSRM <srourr.ali73@gmail.com>
2026-09-16 03:16:56 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,22 @@
+import logging
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
 from keyboards import main_menu
 
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
+    logger.info("START RECEIVED")
+
+    message = update.effective_message
+
+    if message is None:
+        logger.error("No message found")
+        return
+
+    await message.reply_text(
         "🌎 Welcome to UNKNOWN WORLD AI\n\n"
         "🤖 Your AI Super Assistant\n\n"
         "Choose a service from the menu below:",
